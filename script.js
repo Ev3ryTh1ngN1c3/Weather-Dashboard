9a4a5c81a4c7282b3e3d8caa28a521f64e79149d
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2023-11-27 17:35:10 -0500

    updated JavaScript

--- script.js
+++ script.js
@@ -17,11 +17,10 @@ function callAPI(e) {
             let today = data.list[0]
             const now = new Date();
             const currentDateTime = now.toLocaleString();
-            // Current day date should be displayed as well
-            // Also display humidity and wind speed
+
             cityEl.innerText = city
             weather.innerText = today.weather[0].description
-            // Use the date element variable and set it's text to the 'today' variable
+
             temp.innerText = today.main.temp
             iconEl.src = "http://openweathermap.org/img/w/" + today.weather[0].icon + ".png";
             forecastsEl.innerHTML = ""
@@ -30,7 +29,8 @@ function callAPI(e) {
                 let day = data.list[i];
                 let we = day.weather[0].description
                 let temp = day.main.temp
-                // create a variable for the humidity 
+                console.log(day.wind.speed)
+
                 let src = "http://openweathermap.org/img/w/" + today.weather[0].icon + ".png";
                 div.innerHTML = `
        <p>Weather:${we}   </p>
@@ -45,14 +45,3 @@ function callAPI(e) {
             }
         })
 }
-
-//Create a function for saving the city name to local storage
-// Grab the value of input (this will the city)
-// localStorage.setItem([key], [city name]) (You can use an array)
-
-//Create a function for display the cities in local storage
-// create a var and set it to localStorage.getItem([key]) (JSON.parse(localStorage.getItem()[key]) if you use an array)
-// loop through the varaiable you created
-// create a button using document.createElement('button')
-// set the text of the button to the value (button = cityNames[i])
-// append the button to an html element 
