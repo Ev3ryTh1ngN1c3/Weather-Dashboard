ec3d6f56de03a280acd90388fc55aa74ca08438a
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2023-11-27 14:46:12 -0500

    added comments

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
+var cityEl = document.getElementById('cityEl');
+// create a variable for your date element
 function callAPI(e){
     e.preventDefault()
     let city=e.target.country.value
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city+"617296c3e23d4dbf924a899ffb674899")
+  fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=imperial&appid=617296c3e23d4dbf924a899ffb674899")
 .then(res=>res.json())
 .then(data=> {
 
     console.log(data)
     let today=data.list[0]
-
+    // Current day date should be displayed as well
+    // Also display humidity and wind speed
    cityEl.innerText=city
    weather.innerText = today.weather[0].description
+   // Use the date element variable and set it's text to the 'today' variable
    temp.innerText = today.main.temp
    iconEl.src="http://openweathermap.org/img/w/" +  today.weather[0].icon + ".png";
    forecastsEl.innerHTML=""
@@ -19,6 +22,7 @@ function callAPI(e){
       let day=data.list[i];
       let we= day.weather[0].description
       let temp=day.main.temp
+      // create a variable for the humidity 
       let src="http://openweathermap.org/img/w/" +  today.weather[0].icon + ".png";
      div.innerHTML=`
        <p>Weather:${we}   </p>
@@ -33,3 +37,14 @@ function callAPI(e){
    }
 })
 }
+
+//Create a function for saving the city name to local storage
+// Grab the value of input (this will the city)
+// localStorage.setItem([key], [city name]) (You can use an array)
+
+//Create a function for display the cities in local storage
+// create a var and set it to localStorage.getItem([key]) (JSON.parse(localStorage.getItem()[key]) if you use an array)
+// loop through the varaiable you created
+// create a button using document.createElement('button')
+// set the text of the button to the value (button = cityNames[i])
+// append the button to an html element 
