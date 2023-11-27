4494567a27fc003a18e28d4fef18f57a0856e157
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2023-11-27 15:18:59 -0500

    added date & time code

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
 var cityEl = document.getElementById('cityEl');
-// create a variable for your date element
-function callAPI(e){
-    e.preventDefault()
-    let city=e.target.country.value
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=imperial&appid=617296c3e23d4dbf924a899ffb674899")
-.then(res=>res.json())
-.then(data=> {
 
-    console.log(data)
-    let today=data.list[0]
-    // Current day date should be displayed as well
-    // Also display humidity and wind speed
-   cityEl.innerText=city
-   weather.innerText = today.weather[0].description
-   // Use the date element variable and set it's text to the 'today' variable
-   temp.innerText = today.main.temp
-   iconEl.src="http://openweathermap.org/img/w/" +  today.weather[0].icon + ".png";
-   forecastsEl.innerHTML=""
-   for(let i=1;i<data.list.length;i+=8){
-     let div=document.createElement("div")
-      let day=data.list[i];
-      let we= day.weather[0].description
-      let temp=day.main.temp
-      // create a variable for the humidity 
-      let src="http://openweathermap.org/img/w/" +  today.weather[0].icon + ".png";
-     div.innerHTML=`
+function check() {
+    var date = document.getElementById("date").value;
+    var d = new moment(date, 'YYYY-MM-DD');
+    console.log(d)
+}
+
+function callAPI(e) {
+    e.preventDefault()
+    let city = e.target.country.value
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=617296c3e23d4dbf924a899ffb674899")
+        .then(res => res.json())
+        .then(data => {
+
+            console.log(data)
+            let today = data.list[0]
+            const now = new Date();
+            const currentDateTime = now.toLocaleString();
+            // Current day date should be displayed as well
+            // Also display humidity and wind speed
+            cityEl.innerText = city
+            weather.innerText = today.weather[0].description
+            // Use the date element variable and set it's text to the 'today' variable
+            temp.innerText = today.main.temp
+            iconEl.src = "http://openweathermap.org/img/w/" + today.weather[0].icon + ".png";
+            forecastsEl.innerHTML = ""
+            for (let i = 1; i < data.list.length; i += 8) {
+                let div = document.createElement("div")
+                let day = data.list[i];
+                let we = day.weather[0].description
+                let temp = day.main.temp
+                // create a variable for the humidity 
+                let src = "http://openweathermap.org/img/w/" + today.weather[0].icon + ".png";
+                div.innerHTML = `
        <p>Weather:${we}   </p>
        <p>Temprature:${temp}   </p>
        <p>Icon: <img src=${src} /> </p>
      
      `
-     forecastsEl.appendChild(div)
-     
+                forecastsEl.appendChild(div)
 
 
-   }
-})
+
+            }
+        })
 }
 
 //Create a function for saving the city name to local storage
